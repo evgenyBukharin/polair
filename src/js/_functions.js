6766b27b26c4622e78a859ca71d28d22306d7c6b
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2022-10-25 17:50:47 +0500

    init
    initial commit

---
+++ src/js/_functions.js
@@ -0,0 +1,117 @@
+// Данный файл - лишь собрание подключений готовых компонентов.
+// Рекомендуется создавать отдельный файл в папке components и подключать все там
+
+// Определение операционной системы на мобильных
+// import { mobileCheck } from "./functions/mobile-check";
+// console.log(mobileCheck());
+
+// Определение ширины экрана
+// import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
+// if (isDesktop()) {
+//   console.log('...')
+// }
+
+// Троттлинг функции (для ресайза, ввода в инпут, скролла и т.д.)
+// import { throttle } from './functions/throttle';
+// let yourFunc = () => { console.log('throttle') };
+// let func = throttle(yourFunc);
+// window.addEventListener('resize', func);
+
+// Фикс фулскрин-блоков
+// import './functions/fix-fullheight';
+
+// Реализация бургер-меню
+// import { burger } from "./functions/burger";
+
+// Реализация остановки скролла (не забудьте вызвать функцию)
+// import { disableScroll } from './functions/disable-scroll';
+
+// Реализация включения скролла (не забудьте вызвать функцию)
+// import { enableScroll } from './functions/enable-scroll';
+
+// Реализация модального окна
+// import GraphModal from 'graph-modal';
+// const modal = new GraphModal();
+
+// Реализация табов
+// import GraphTabs from 'graph-tabs';
+// const tabs = new GraphTabs('tab');
+
+// Получение высоты шапки сайта (не забудьте вызвать функцию)
+// import { getHeaderHeight } from './functions/header-height';
+
+// Подключение плагина кастом-скролла
+// import 'simplebar';
+
+// Подключение плагина для позиционирования тултипов
+// import { createPopper, right} from '@popperjs/core';
+// createPopper(el, tooltip, {
+//   placement: 'right'
+// });
+
+// Подключение свайпера
+// import Swiper, { Pagination, Autoplay, Navigation } from "swiper";
+// Swiper.use([Pagination, Autoplay, Navigation]);
+// const swiper = new Swiper(".swiper", {
+// 	speed: 800,
+// 	spaceBetween: 10,
+// 	grabCursor: true,
+// 	slideClass: "swiper-slide",
+// 	slideActiveClass: "hero__slide-active",
+// 	slidesPerView: 1,
+// 	navigation: {
+// 		nextEl: ".hero__btn-next",
+// 		prevEl: ".hero__btn-prev",
+// 		disabledClass: "hero__btn-disabled",
+// 	},
+// 	pagination: {
+// 		el: ".swiper-pagination",
+// 		bulletClass: "hero__bullet",
+// 		bulletActiveClass: "hero__bullet-active",
+// 		clickable: true,
+// 		renderBullet: function (index, className) {
+// 			return `<li class="${className}" tabindex="0">${paginationElems[index]}</li>`;
+// 		},
+// 	},
+// 	on: {
+// 		init: () => {
+// 			swiperSlides.forEach((slide) => {
+// 				slide.inert = true;
+// 			});
+// 			swiperSlides[defaultActiveSlide].inert = false;
+// 		},
+// 		slideChange: () => {
+// 			swiperSlides[swiper.activeIndex].inert = false;
+// 			swiperSlides[swiper.previousIndex].inert = true;
+// 		},
+// 	},
+// });
+
+// Подключение анимаций по скроллу
+// import AOS from 'aos';
+// AOS.init();
+
+// Подключение параллакса блоков при скролле
+// import Rellax from 'rellax';
+// const rellax = new Rellax('.rellax');
+
+// Подключение плавной прокрутки к якорям
+// import SmoothScroll from 'smooth-scroll';
+// const scroll = new SmoothScroll('a[href*="#"]');
+
+// Подключение событий свайпа на мобильных
+// import 'swiped-events';
+// document.addEventListener('swiped', function(e) {
+//   console.log(e.target);
+//   console.log(e.detail);
+//   console.log(e.detail.dir);
+// });
+
+// import { validateForms } from './functions/validate-forms';
+// const rules1 = [...];
+
+// const afterForm = () => {
+//   console.log('Произошла отправка, тут можно писать любые действия');
+// };
+
+// validateForms('.form-1', rules1, afterForm);
